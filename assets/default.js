f0b0167bf5eb50ca75abb12c84d3116535bf7042
Tayler Geiger <mail@tayler.me>
2018-10-31 14:18:44 -0500

    updated font awesome and basically refigured the entire menu system for mobile to an overlay

--- assets/default.js
+++ assets/default.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
-  
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 165) {
-      $('.site-header').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 165) {
-      $('.site-header').removeClass('navbar-fixed');
-    }
-  });
+
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 165) {
+            $('.site-header').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < 165) {
+            $('.site-header').removeClass('navbar-fixed');
+        }
+    });
+
+
+    $(".open, .overlay").click(function() {
+        if ($(window).width() < 1130) {
+            $(".overlay").fadeToggle(300);
+            $(".open").toggleClass("close");
+        };
+    });
+
+    $(".nav_items a").on('click', function() {
+        if ($(window).width() < 1130) {
+            $(".overlay").fadeToggle(300);
+            $(".open").toggleClass("close");
+        };
+    });
 });
